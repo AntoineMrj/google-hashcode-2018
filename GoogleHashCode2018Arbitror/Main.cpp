2a23f99f5d24371ae43918bbb8f1847adc9ccabc
Simon BESSENAY <simon.bessenay@etu.univ-nantes.fr>
2018-12-10 18:07:48 +0100

    Début fonction main pour checker validité des arguments

--- GoogleHashCode2018Arbitror/Main.cpp
+++ GoogleHashCode2018Arbitror/Main.cpp
@@ -1,13 +1,43 @@
-// GoogleHashCode2018Arbitror.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
-
 #include "Arbitror.h"
 #include <iostream>
+#include <sys/stat.h>
 
-int main()
+using namespace std;
+
+int main(int argc, char **argv)
 {
+	// We check the number of arguments
+	if (argc == 3)
+	{
+		const char * executableDirectory;
+		const char * outputFile;
+		executableDirectory = argv[1];
 
-    std::cout << "Hello World!\n"; 
+		struct stat buff;
+		// We check if the second argument is a directory
+		if (stat(executableDirectory, &buff) == 0)
+		{
+			if (!(buff.st_mode & S_IFDIR))
+			{
+				cout << "Error: the second argument is not a directory" << endl;
+				cout << "Usage: ./program executablesDirectory outputFile" << endl;
+				exit(0);
+			}
+		}
+		else
+		{
+			cout << "Error while checking if the second argument is a directory" << endl;
+			exit(0);
+		}
+
+		outputFile = argv[2];
+
+	}
+	else
+	{
+		cout << "Usage: ./program executablesDirectory outputFile" << endl;
+		exit(0);
+	}
 }
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
