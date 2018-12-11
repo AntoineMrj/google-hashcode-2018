ed4cff1a454af0dd09fe553a1016affb57e92275
Simon BESSENAY <simon.bessenay@etu.univ-nantes.fr>
2018-12-11 16:59:31 +0100

    Modif main arbitror pour créer un vecteur comportant les temps d'exécutions et scores

--- GoogleHashCode2018Arbitror/Arbitror.cpp
+++ GoogleHashCode2018Arbitror/Arbitror.cpp
@@ -2,32 +2,17 @@
 
 using namespace std;
 
+Arbitror::Arbitror()
+{
+}
+
 Arbitror::Arbitror(string projectFile, string solutionFile)
 {
 	this->projectFile = projectFile;
 	this->solutionFile = solutionFile;
 	this->score = 0;
 	this->fileLoader = new FileLoader;
-	this->computeTime();
-}
-
-/*
-	Method that calculates the execution time of the solution's score computation
-*/
-double Arbitror::computeTime()
-{
-	auto start = chrono::steady_clock::now();
-
-	//Creation of objects corresponding to a project and a solution of it
-	this->city = fileLoader->loadSolution(projectFile, solutionFile);
-	//Computation of the solution's score
 	this->evaluate();
-
-	auto end = chrono::steady_clock::now();
-
-	// Execution time
-	chrono::duration<double> executionTime = end - start;
-	return executionTime.count();
 }
 
 /*
@@ -36,6 +21,11 @@ double Arbitror::computeTime()
 void Arbitror::evaluate()
 {
 	//TODO
+}
+
+int Arbitror::getScore()
+{
+	return this->score;
 }
 
 Arbitror::~Arbitror()
--- GoogleHashCode2018Arbitror/Main.cpp
+++ GoogleHashCode2018Arbitror/Main.cpp
@@ -1,9 +1,34 @@
 #include "Arbitror.h"
+#include "FileHandling.h"
 #include <iostream>
 #include <sys/stat.h>
+#include <chrono>
+#include <Windows.h>
+#include <utility>
+
+#define NUMBER_EXEC 3
 
 using namespace std;
 
+/*
+	Method that calculates the execution time of the solution's computation
+*/
+double computeExecutionTime(const string command)
+{	
+	auto start = chrono::steady_clock::now();
+	// We execute the executable
+	system(command.c_str());
+	auto end = chrono::steady_clock::now();
+
+	// Execution time
+	chrono::duration<double> executionTime = end - start;
+
+	return executionTime.count();
+}
+
+/*
+	Main function
+*/
 int main(int argc, char **argv)
 {
 	// We check the number of arguments
@@ -13,29 +38,41 @@ int main(int argc, char **argv)
 		const char * outputFile;
 		executableDirectory = argv[1];
 
-		struct stat buff;
 		// We check if the second argument is a directory
-		if (stat(executableDirectory, &buff) == 0)
+		if (!((GetFileAttributesA(executableDirectory) & FILE_ATTRIBUTE_DIRECTORY) != 0))
 		{
-			if (!(buff.st_mode & S_IFDIR))
-			{
-				cout << "Error: the second argument is not a directory" << endl;
-				cout << "Usage: ./program executablesDirectory outputFile" << endl;
-				exit(0);
-			}
-		}
-		else
-		{
-			cout << "Error while checking if the second argument is a directory" << endl;
+			cout << "Error: the second argument is not a directory" << endl;
+			cout << "Usage: " << argv[0] << " [executablesDirectory] [outputFile]" << endl;
 			exit(0);
 		}
 
 		outputFile = argv[2];
+		
+		//We iterate through the directory
+		vector<string> executables;
+		executables = IterateOnFileDir(executableDirectory);
+
+		Arbitror arbitror;
+		vector<pair<double, int>> result;
+
+		for (auto file : executables)
+		{
+			arbitror = Arbitror("projectFile ?????", "solution");
+			double mean = 0;
+			const string command = file + " solution";
+			for (int i = 0; i < NUMBER_EXEC; i++)
+			{
+				mean += computeExecutionTime(command);
+			}
+			result.push_back(make_pair(mean / NUMBER_EXEC, arbitror.getScore()));
+		}
+		
+		//TODO: sort results + print it in file
 
 	}
 	else
 	{
-		cout << "Usage: ./program executablesDirectory outputFile" << endl;
+		cout << "Usage: " << argv[0] << " [executablesDirectory] [outputFile]" << endl;
 		exit(0);
 	}
 }
