6fc40c73ace2395831a372c79b4fadd32749ba4c
Nicolas CUADROS <nicolas.cuadros@etu.univ-nantes.fr>
2018-12-12 17:07:53 +0100

    Adding arguments for the program.

--- GoogleHashCode2018/main.cpp
+++ GoogleHashCode2018/main.cpp
@@ -4,7 +4,22 @@
 int main(int argc, char* argv[])
 {
 
-	FileLoader::loadProject("GoogleHashCode2018/input/a_example.in");
+	if(argc<3)
+	{
+		std::cerr << "Erreur : Too fee arguments." << std::endl;
+		std::cout << "This program require 2 files path as input, first one for the project path, the second one for the output path." << std::endl;
+	}
+	else if(argc>3)
+	{
+		std::cerr << "Erreur : Too much arguments." << std::endl;
+		std::cout << "This program require 2 files path as input, first one for the project path, the second one for the output path." << std::endl;
+	}
+	else
+	{
+		std::string projectPath(argv[1]);
+		std::string solutionOuput(argv[2]);
+		FileLoader::loadProject(argv[1]);
+	}
 
 	return EXIT_SUCCESS;
 }
