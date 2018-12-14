ae540fb61708407b4520aa1135c22c5f3a891059
sebastien <sebastien.berlioux@etu.univ-nantes.fr>
2018-12-14 17:59:30 +0100

    MAJ solver

--- GoogleHashCode2018/Solver.cpp
+++ GoogleHashCode2018/Solver.cpp
@@ -1,36 +1,49 @@
 #include "Solver.h"
 #include <stdlib.h>
+#include <time.h>
 
 Solver::Solver(Project project)
 {
 	unsigned int x, y;
 	unsigned int x_tmp, y_tmp;
 	PlacedBuilding B_tmp, A;
-	int score_max = -1;
+	int score_max_building = -1;
 	int score_tot = 0;
 	int score = 0;
 	size_t c = 0;
+	bool first = true;
+
+	srand(time(NULL));
 
 	for (Building *B : project.buildings)
 	{
-		while (c != 10) // on essaie 10 position différentes
-		{
-			x_tmp = rand() % (project.city->getCityHeight()-1) + 0;
-			y_tmp = rand() % (project.city->getCityWidth()-1) + 0;
-			
-			project.city->placeBuilding(B, x_tmp, y_tmp, true);
-			B_tmp = B;
-
-			if ((score = project.city->computeScore(B_tmp, A)) > score_max) {
-				x = x_tmp;
-				y = y_tmp;
-				score_max = score;
-			}
-			c++;
+		if (first) {
+			project.city->placeBuilding(B, 0, 0, false);
+			first = false;
 		}
-		project.city->placeBuilding(B, x, y, false);
-		score_tot += score_max;
-		project.city->setScore(score_tot);
+		else {
+			B_tmp = B;
+			while (c!=10) // on essaie 10 positions différentes
+			{
+				x_tmp = rand() % project.city->getCityHeight();
+				y_tmp = rand() % project.city->getCityWidth();
+
+				project.city->placeBuilding(B, x_tmp, y_tmp, true);
+				score = project.city->computeScore(B_tmp, A);
+
+				if (score > score_max_building) {
+					x = x_tmp;
+					y = y_tmp;
+					score_max_building = score;
+				}
+				c++;
+			}
+			project.city->placeBuilding(B, x, y, false);
+			score_tot += score_max_building;
+			project.city->setScore(score_tot);
+			score_max_building = -1;
+			c = 0;
+		}
 		A = B;
 	}
 }
